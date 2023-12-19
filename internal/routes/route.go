2f380b1157f070f68722299820307c7190abcfbf
abulo.hoo <abulo.hoo@gmail.com>
2023-12-19 15:06:45 +0800

    feat✨: route

--- internal/routes/route.go
+++ internal/routes/route.go
@@ -1,9 +1,15 @@
 package routes
 
 import (
+	"context"
+
 	"github.com/abulo/ratel/v3/server/xhertz"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
 
-func InitRoute(h *xhertz.Server) {
-
+func InitRoute(handle *xhertz.Server) {
+	handle.GET("/get", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "get")
+	})
 }
